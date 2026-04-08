1aecc36f090e8a4399bf131d5a731aaea4aada0b
shyu216 <yusihong073@gmail.com>
2026-04-08 13:39:29 +1000

    update

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/',
+  base: '/YeluYelu/',
   publicDir: 'public',
   build: {
     outDir: 'dist',
